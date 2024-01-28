d71c6e8c92583816306f994b3b4b51f31c08cdd0
Artyom Hakobyan <aghakobyan@gmail.com>
2024-01-28 04:42:08 +0400

    Add small delay in EnemyAttack state before hurting player.

--- src/combat/AI/EnemyFSMStates/EnemyAttack.gd
+++ src/combat/AI/EnemyFSMStates/EnemyAttack.gd
@@ -6,6 +6,7 @@ class_name EnemyAttack
 @export var attack_distance: float = 30.0
 
 @onready var timer: Timer = $Timer
+@onready var hurt_delay_timer: Timer = $HurtDelayTimer
 
 func enter():
 	super()
@@ -15,6 +16,7 @@ func enter():
 func exit():
 	super()
 	timer.stop()
+	hurt_delay_timer.stop()
 
 func update(_delta: float):
 	super(_delta)
@@ -27,5 +29,7 @@ func hit_player():
 	var to_target = charBody.to_local(charBody.player.global_position)
 	charBody.look_direction(to_target)
 	anim.play("attack")
-	attackNode.attack()
+	hurt_delay_timer.start()
 
+func damage_player():
+	attackNode.attack()
